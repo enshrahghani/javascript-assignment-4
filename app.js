27ac3d0fde6fe37e6383cc2a63c0a32abbf5bc04
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-11-14 12:59:45 +0500

    javascript assignment 4

---
+++ app.js
@@ -0,0 +1,189 @@
+// chapter 01
+
+alert("Error! Please enter a valid password")
+
+
+var line1 ="welcome to js land..."
+var line2 ="happy coding!"
+var message =line1+"\n"+line2
+
+    alert(message)
+
+alert("welcome to js land...")
+
+
+var line1 ="happy coding!"
+var line2 ="Prevent this page from creating additional dialogs"
+var message =line1+"\n"+line2
+
+    alert(message)
+
+alert("Hello... I can run JS through my web broweser's console")
+console.log("Hello... I can run JS through my web broweser's console")
+
+// // chapter 02
+
+var username = "enshrah";
+console.log(username)
+
+
+alert("hello world")
+alert("Enshrah")
+alert("I'm 18 years old")
+alert("Certified mobile application development")
+
+var line1 ="PIZZA"
+var line2 ="PIZZ"
+var line3 ="PIZ"
+var line4 ="PI"
+var line5 ="P"
+
+var message =line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5
+alert(message)
+
+
+alert("my email address is enshrahabdulghani@gmail.com")
+alert("I am trying to learn from the book A smarter way to learn javcascript")
+alert("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+document.write("Yah! I can write html content through javascript")
+
+// // chapter 03
+
+var age = ("I'm 18 years old")
+alert(age)
+
+alert("You have visited this tsite 14 times")
+
+document.write("My birth year is 2004 <br/>")
+document.write("data type of my declared variable is number")
+
+document.write("<br><b>John Doe</b>Oredered <b>5 T-shirts</b>(s)on XYZ clothing store")
+
+// // chapter 04
+
+var statement01 = "hey there!"
+var statement02 = "My name is enshrah"
+var statement03 = "and I'm 18 years old"
+
+alert(statement01 + statement02 + statement03)
+
+// legal                  illegal
+// var abc                var @123
+// var abc123             var alert
+// var _123               var -abc
+// var _abc               var 89
+
+
+document.write("<br><h2>Rules for naming JS variables</h2>")
+document.write("<br>Variable must begin with a letter $ or _. for example @name _1st variablbe")
+document.write("<br>variable names are case sensitive")
+document.write("<br>variable names should not be JS keywords")
+
+// // chapter 05
+
+var a = 5 + 3
+console.log(a)
+
+var b = 5 - 3
+console.log(b)
+
+var c = 5 * 3
+console.log(c)
+
+var d = 5 / 3
+console.log(d)
+
+var a = 4
+a++
+console.log(a)
+
+var b = 5
+++b
+console.log(b)
+
+var c = 7 + 6
+console.log(c)
+
+var d = 13
+d--
+console.log(d)
+
+var e = 18 / 6
+console.log(e)
+
+var f = 2 - 2
+console.log(f)
+
+var g = 600 * 5
+console.log(g)
+
+
+for (var i = 0; i < 10; i++){
+    console.log("12 X" + (i + 1) + "=" + 12* (i + 1))
+}
+
+
+var ab = 650 * 3 + 100 * 7 + 100
+console.log(ab)
+
+var cd = 650 / 800 * 100
+console.log(cd + "%")
+
+var ef = 10 * 104.80 + 25 * 28
+console.log(ef)
+
+var gh = (4 + 1) * 10 / 2
+console.log(gh)
+
+var ij = 2022 - 2004
+console.log(ij)
+
+var kl = (65 - 15) * 3
+console.log(kl)
+
+// chapter 6-9
+
+var mn = 10
+mn++
+console.log(mn)
+
+var op = 11
+++op 
+console.log(op)
+
+var qr = 12
+qr--
+console.log(qr)
+
+var st = 11
+--st
+console.log(st)
+
+var a = 2
+var b = 1 
+var c = --a - --b + ++b + b--
+console.log(c)
+
+var d = prompt("welcome", "enshrah")
+alert(d)     
+
+var num = prompt("Enter Table number")
+for (var table = 0; table < 10; table++){
+    console.log(num+"X" + (table + 1) + " = " + num * (table + 1))
+}
+
+
+var subject1 = "english"
+var subject2 = "math"
+var subject3 = "urdu"
+
+var subject1 = 100
+var subject2 = 100
+var subject3 = 100
+
+var subject1 = 80
+var subject2 = 75
+var subject3 = 90
+
+
+var total = 80 / 100 * 100
